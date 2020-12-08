9e184420664636295755f58995c580aff04caa87
Hamish Darbyshire <hamish.darbyshire@fcg.se>
2020-12-08 11:20:12 +0100

    docs: add new

---
+++ documentation/source/index.rst
@@ -0,0 +1,21 @@
+.. Project A documentation master file, created by
+   sphinx-quickstart on Mon Dec  7 12:01:37 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Project A's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Contents:
+
+   functions.rst
+   notes.rst
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- modules/functions.py
+++ modules/functions.py
@@ -1,8 +1,52 @@
+"""
+=====================
+Functions Description
+=====================
+
+Here we can type a description of the project
+
+Sub heading
+===========
+
+paragraph.
+
+Classes and Methods
+===================
+"""
+
+
 def add_ten(param):
-    """Added docstring by supposed alternate collaborator"""
+    """
+    Very brief description of what it does. I.e adds ten.
+
+    Args:
+        param (float): The input number
+
+    Returns:
+        float: The output number
+
+    Notes:
+        This is great.
+
+        .. image:: _static/sphinx.jpg
+            :align: center
+
+        .. math::
+
+            \\alpha =& \\beta \\\\
+            \\gamma =& \\int_0^x s .ds \\\\
+
+        .. code-block:: python
+
+            import pandas as pd
+            df = pd.DataFrame()
+
+        .. note:: A note here
+        .. warning:: Some warning here!!!
+    """
     return param + 10
 
 
-def add_one(param):
+def _add_one(param):
     """add new changes"""
     return param + 1
